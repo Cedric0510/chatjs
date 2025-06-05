05bef73a8492cc81dd2d385f08abecc7c7f7ede5
cedric <cedric>
2025-06-05 10:26:29 +0200

    register + login + hash password + Bcrypt Ok

--- src/app/routes/users.js
+++ src/app/routes/users.js
@@ -8,6 +8,8 @@ router.get("/:id", (req, res) => UserController.getById(req, res));
 // router.post("/", (req, res) => UserController.create(req, res));
 router.delete("/:id", (req, res) => UserController.delete(req, res));
 router.get("/:id/rooms", (req, res) => UserController.getMyRooms(req, res));
-router.post("/login", (req, res) => UserController.loginOrCreate(req, res));
+// router.post("/login", (req, res) => UserController.loginOrCreate(req, res));
+router.post("/register", (req, res) => UserController.register(req, res));
+router.post("/login", (req, res) => UserController.login(req, res));
 
 export default router;
